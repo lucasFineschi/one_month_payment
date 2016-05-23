aea985d2811056a9cc6932b33f72f05068e43a59
Lucas Fineschi <lucas.fineschi@gmail.com>
2016-05-23 10:42:00 -0300

    Added uuid for security in each purchase in the url

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,10 +16,10 @@ class ChargesController < ApplicationController
 
   purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
     amount: params[:amount], description: charge.description, currency: charge.currency,
-    customer_id: customer.id, product_id: 1)
+    customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
 
 redirect_to purchase
-  
+
 	rescue Stripe::CardError => e
   	flash[:error] = e.message
   	redirect_to charges_path
