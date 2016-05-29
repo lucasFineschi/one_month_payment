40ddf5ac5590058f79c85adc736ff4acbe78b074
Lucas Fineschi <lucas.fineschi@gmail.com>
2016-05-29 20:36:01 -0300

    Changin

--- db/migrate/20160529202205_create_products.rb
+++ db/migrate/20160529202205_create_products.rb
@@ -2,12 +2,17 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
     	t.string :title
-    	t.string :author
-    	t.text :description
-    	t.decimal :price
-    	t.string :download_url
-    	t.string :sku
-      t.timestamps null: false
+      t.string :subtitle
+      t.string :author
+      t.string :author_image_name
+      t.decimal :price
+      t.string :sku
+      t.string :length
+      t.string :download_url
+      t.string :details
+      t.text :description
+      t.string :author_description
+      t.timestamps
     end
   end
 end
