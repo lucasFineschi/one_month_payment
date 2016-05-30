539b299db39d0ef3d1cbab6ad21051eb7ff97438
Lucas Fineschi <lucas.fineschi@gmail.com>
2016-05-29 21:09:03 -0300

    Master equals to branch

---
+++ db/migrate/20160529202205_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+    	t.string :title
+      
+      t.string :author
+      
+      t.decimal :price
+      t.string :sku
+      
+      t.text :description
+      
+      t.timestamps
+    end
+  end
+end
